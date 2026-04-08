ae2e2ca2c8b6c6aa69dce9004ddf190004000eb1
Alessandro De Bonis <alessdebonis@gmail.com>
2026-04-08 14:01:36 -0500

    Actualizar SumaArreglo

--- SumaArreglo.cpp
+++ SumaArreglo.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+
+int addElements (int* arrayNum, int arraySize) {
+    int sum = 0;
+    for (int i = 0; i < arraySize; i++) {
+        sum = sum + arrayNum[i];
+        
+    }
+    return sum;
+}
+
 int main () {
 
     int arraySize = 0;
@@ -8,18 +18,22 @@ int main () {
     cout<< "Ingrese el tamaño de la lista de numeros: ";
     cin >> arraySize;
 
-    int* array = new (nothrow) int [arraySize];
+    int* arrayNum = new (nothrow) int [arraySize];
 
-    cout << "Ingrese los valores de la lista de numeros: \n"
+    cout << "Ingrese los valores de la lista de numeros : \n";
     
     for (int i = 0; i < arraySize; i++) {
 
-        cout << "Numero" << i + 1 <<
-
-
+        cout << "Numero" << i + 1 << ": ";
+        cin >> arrayNum[i];
     }
 
+    int sum = addElements(arrayNum, arraySize);
 
+    cout<< "La suma de los numeros es: "<< sum << "\n";
+
+
+    delete[] arrayNum;
 
 
     return 0;
